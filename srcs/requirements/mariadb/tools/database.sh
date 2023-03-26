6653cbb5fe643a4513d219e7faf86d2b4c2c668b
Mustapha Mardi <mmardi@e2r4p1.1337.ma>
2023-03-26 18:23:43 +0000

    nothin works

--- srcs/requirements/mariadb/tools/database.sh
+++ srcs/requirements/mariadb/tools/database.sh
@@ -8,8 +8,10 @@ mariadb -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 
 mariadb -e "CREATE USER '$USER_NAME'@'%' IDENTIFIED BY '$DB_PSWD';"
 
-mariadb -e "GRANT ALL PRIVILEGES ON * . * TO '$USER_NAME'@'localhost';"
+mariadb -e "GRANT ALL PRIVILEGES ON * . * TO '$USER_NAME'@'%';"
 
 mariadb -e "FLUSH PRIVILEGES"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 mysqld
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-wget http://wordpress.org/latest.tar.gz
+sleep 15
 
-tar xfz latest.tar.gz
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
-mv wordpress/* ./var/www/html/
+chmod +x wp-cli.phar
 
-rm -f latest.tar.gz /var/www/html/index.html
+mv wp-cli.phar usr/local/bin/wp 
+
+wp core download  --path="/var/www/html/" --allow-root
+
+rm -f /var/www/html/index.html
 
 cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 
@@ -16,3 +20,9 @@ sed -i "s/username_here/$USER_NAME/" /var/www/html/wp-config.php
 
 sed -i "s/password_here/$DB_PSWD/" /var/www/html/wp-config.php
 
+wp user create mmardiwp mmardiwp@example.com role=author --path="/var/www/html/" -user_pass=1234 --allow-root
+sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+
+mkdir run/php
+echo "creat run/php floder"
+echo "star wordpress" && /usr/sbin/php-fpm7.3 -F 
