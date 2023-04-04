ff25d3cfb52f06f90bc4367cd377aaaadf7a2ec4
Mustapha Mardi <mmardi@e2r5p9.1337.ma>
2023-04-04 00:02:35 +0000

    last whisper

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sleep 15
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
@@ -12,15 +11,11 @@ wp core download  --path="/var/www/html/" --allow-root
 
 rm -f /var/www/html/index.html
 
-cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+wp config create --dbname=$DB_NAME --dbuser=$USER_NAME --dbpass=$DB_PSWD --dbhost=localhost  --path="/var/www/html" --allow-root
+wp core install --url=localhost/ --title=mmardi --admin_user=$USER_NAME --admin_password=$DB_PSWD --admin_email=mmardi@gmail.com --skip-email  --path="/var/www/html" --allow-root
 
-sed -i "s/database_name_here/$DB_NAME/" /var/www/html/wp-config.php
 
-sed -i "s/username_here/$USER_NAME/" /var/www/html/wp-config.php
-
-sed -i "s/password_here/$DB_PSWD/" /var/www/html/wp-config.php
-
-wp user create mmardiwp mmardiwp@example.com role=author --path="/var/www/html/" -user_pass=1234 --allow-root
+# wp user create mmardiwp mmardiwp@example.com role=author --path="/var/www/html/" -user_pass=1234 --allow-root
 sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 
 mkdir run/php
